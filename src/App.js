a925ed6b06c9d08b8ba904f1e5807c1850283167
Mehrnaz-zand <zand.mehrnaz@gmail.com>
2021-11-21 14:10:12 +0100

    Added Github repo url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>
+
      Weather app
+     </h1>
+    <footer>
+      This project was created by {" "} 
+      <a href="https://www.linkedin.com/in/mehrnaz-zand" target="_blank" rel="noreferrer">Mehrnaz Zand </a>
+       and is {" "}
+       <a href="https://github.com/Mehrnaz-zand/react-weather-app-second-version" target="_blank" rel="noreferrer">
+         open-sourced on Github
+      </a>
+      
+    </footer>
     </div>
   );
 }
